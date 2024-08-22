a868ec9dd0fbd6533637f7b6c75fa1bafde46ffc
康营营 <1031041331@qq.com>
2024-08-22 10:48:28 +0800

    code review: 代码review, 为pdf生成html span标签增加id

--- src/views/pdf/PDFRender.ts
+++ src/views/pdf/PDFRender.ts
@@ -1,62 +1,53 @@
 import * as PDF from 'pdfjs-dist'
-import 'pdfjs-dist/web/pdf_viewer.css'
+
 import {
   EventBus,
   PDFFindController,
-  PDFLinkService, PDFPageView,
+  PDFLinkService,
+  PDFPageView,
   PDFScriptingManager,
   PDFViewer
 } from 'pdfjs-dist/web/pdf_viewer.mjs'
-import 'pdfjs-dist/build/pdf.worker.mjs' // 此处import 解决控制台报错 No "GlobalWorkerOptions.workerSrc" specified.
+import 'pdfjs-dist/build/pdf.worker.mjs'
+import type { PDFDocumentProxy } from 'pdfjs-dist'
 
-export default function(props: any) {
+export default function(props?: any) {
   // 此处不用响应式 解决 vue3  Cannot read from private field问题
-  let pdfDoc: any
-  let pdfViewer: PDFViewer
-
+  let pdfDoc: PDFDocumentProxy,
+      pdfViewer: PDFViewer,
+      eventBus: EventBus,
+      pdfLinkService: PDFLinkService,
+      pdfFindController: PDFFindController,
+      pdfScriptingManager: PDFScriptingManager
   /**
    * 加载pdf
    * @param url
-   * @param searchText
+   * @param SEARCH_FOR
    */
-  const renderPDF = (url?: string, searchText?: string) => {
-    const SEARCH_FOR = searchText || ''; // try "Mozilla";
-
-    const container: any = document.getElementById(props.id);
-
-    const eventBus = new EventBus();
-
+  const renderPDF = (url?: string, SEARCH_FOR?: string) => {
+    SEARCH_FOR = SEARCH_FOR || ''; // try "Mozilla";
+    // pdf渲染容器
+    const container: any = document.getElementById(props.id)
+    eventBus = new EventBus();
     // (Optionally) enable hyperlinks within PDF files.
-    const pdfLinkService = new PDFLinkService({eventBus});
-
+    pdfLinkService = new PDFLinkService({eventBus});
     // (Optionally) enable find controller.
-    const pdfFindController = new PDFFindController({eventBus,linkService: pdfLinkService,});
-
+    pdfFindController = new PDFFindController({eventBus,linkService: pdfLinkService,});
     // (Optionally) enable scripting support.
-    const pdfScriptingManager = new PDFScriptingManager({eventBus}); // ,// sandboxBundleSrc: SANDBOX_BUNDLE_SRC,
-
+    pdfScriptingManager = new PDFScriptingManager({eventBus}); // ,// sandboxBundleSrc: SANDBOX_BUNDLE_SRC,
+    // TODO 初始化缩略图Layer
     pdfViewer = new PDFViewer({
       container,
       eventBus,
       linkService: pdfLinkService,
       findController: pdfFindController,
       scriptingManager: pdfScriptingManager,
+      removePageBorders: true, // page获取焦点时不展示边框
     });
-
     pdfLinkService.setViewer(pdfViewer);
     pdfScriptingManager.setViewer(pdfViewer);
-
-    eventBus.on("pagesinit", function () {
-      console.log(pdfViewer)
-
-      // We can use pdfViewer now, e.g. let's change default scale.
-      pdfViewer.currentScaleValue = "page-width";
-      // We can try searching for things.
-      if (SEARCH_FOR) {
-        // 搜索关键字高亮
-        eventBus.dispatch("find", { type: "", query: SEARCH_FOR, highlightAll: true});
-      }
-    });
+    // 初始化事件监听
+    initEventBus(SEARCH_FOR)
     // Loading document.
     PDF.getDocument(url).promise.then(async (pdfDocument) => {
       pdfDoc = pdfDocument;
@@ -65,66 +56,111 @@ export default function(props: any) {
     });
   }
 
-  const renderPDFPage = async (url?: string) => {
+  // /**
+  //  * 加载pdf page(单页加载)
+  //  * @param url
+  //  */
+  // const renderPDFPage = async (url?: string) => {
+  //   const PAGE_TO_VIEW = 1;
+  //   const SCALE = 0.7;
+  //
+  //   const ENABLE_XFA = true;
+  //
+  //   const container = document.querySelector(`#${props.id} .pdfViewer`) as HTMLDivElement;
+  //
+  //   const eventBus = new EventBus();
+  //
+  //   eventBus.on("pagesinit", function () {
+  //     console.log('pagesinit===============>>')
+  //     // We can use pdfViewer now, e.g. let's change default scale.
+  //     pdfViewer.currentScaleValue = "page-width";
+  //
+  //     // // We can try searching for things.
+  //     // if (SEARCH_FOR) {
+  //     //   eventBus.dispatch("find", { type: "", query: SEARCH_FOR });
+  //     // }
+  //   });
+  //
+  //   // Loading document.
+  //   pdfDoc = await  PDF.getDocument(url).promise;
+  //   // Document loaded, retrieving the page.
+  //   const pdfPage = await getPDFPage(PAGE_TO_VIEW);
+  //
+  //   console.log(await pdfPage.getTextContent())
+  //
+  //   // Creating the page view with default parameters.
+  //   const pdfPageView = new PDFPageView({
+  //     container,
+  //     id: PAGE_TO_VIEW,
+  //     scale: SCALE,
+  //     defaultViewport: pdfPage.getViewport({ scale: SCALE }),
+  //     eventBus,
+  //   });
+  //   // Associate the actual page with the view, and draw it.
+  //   pdfPageView.setPdfPage(pdfPage);
+  //
+  //   await pdfPageView.draw();
+  // }
+  //
+  // const getPDFPage = (num: any): Promise<any>  => {
+  //   return new Promise((resolve) => {
+  //      pdfDoc.getPage(num).then((pdfPage: any) => {
+  //        pdfPage.getTextContent().then((textContent: any) => {
+  //          textContent.items.forEach((item: any) => {
+  //            if (item.str === 'Languages') {
+  //              item.str = '语言';
+  //            }
+  //          });
+  //          // pdfPage.textContent = textContent;
+  //          // pdfPage.cleanup()
+  //          resolve(pdfPage)
+  //        });
+  //     })
+  //
+  //
+  //   })
+  // }
 
-    const PAGE_TO_VIEW = 1;
-    const SCALE = 0.7;
-
-    const ENABLE_XFA = true;
-
-    const container = document.querySelector(`#${props.id} .pdfViewer`) as HTMLDivElement;
-
-    const eventBus = new EventBus();
-
+  /**
+   * 初始化事件监听
+   * @param SEARCH_FOR
+   */
+  const initEventBus = (SEARCH_FOR?: string) => {
+    // 页面加载完成事件监听
     eventBus.on("pagesinit", function () {
-      console.log('pagesinit===============>>')
       // We can use pdfViewer now, e.g. let's change default scale.
       pdfViewer.currentScaleValue = "page-width";
-
-      // // We can try searching for things.
-      // if (SEARCH_FOR) {
-      //   eventBus.dispatch("find", { type: "", query: SEARCH_FOR });
-      // }
+      // We can try searching for things.
+      if (SEARCH_FOR) {
+        // 搜索关键字高亮
+        eventBus.dispatch("find", { type: "", query: SEARCH_FOR, highlightAll: true});
+      }
     });
 
-    // Loading document.
-    pdfDoc = await  PDF.getDocument(url).promise;
-    // Document loaded, retrieving the page.
-    const pdfPage = await getPDFPage(PAGE_TO_VIEW);
-
-    console.log(await pdfPage.getTextContent())
-
-    // Creating the page view with default parameters.
-    const pdfPageView = new PDFPageView({
-      container,
-      id: PAGE_TO_VIEW,
-      scale: SCALE,
-      defaultViewport: pdfPage.getViewport({ scale: SCALE }),
-      eventBus,
-    });
-    // Associate the actual page with the view, and draw it.
-    pdfPageView.setPdfPage(pdfPage);
-
-    await pdfPageView.draw();
+    // 每页textLayer渲染完成事件监听
+    eventBus.on('textlayerrendered', (event: any) => {
+      // 为每个span增加id
+      setTextSpanId(event);
+    })
   }
 
-  const getPDFPage = (num: any): Promise<any>  => {
-    return new Promise((resolve) => {
-       pdfDoc.getPage(num).then((pdfPage: any) => {
-         pdfPage.getTextContent().then((textContent: any) => {
-           textContent.items.forEach((item: any) => {
-             if (item.str === 'Languages') {
-               item.str = '语言';
-             }
-           });
-           // pdfPage.textContent = textContent;
-           // pdfPage.cleanup()
-           resolve(pdfPage)
-         });
-      })
+  /**
+   * 为span增加id
+   * @param event
+   */
+  const setTextSpanId = (event: any) => {
+    const pdfPageView = event.source as PDFPageView;
+    const pageRenderId = pdfPageView.renderingId;
+    const textLayerContainer = pdfPageView.textLayer?.div;
+    const textNodes: any = textLayerContainer?.children
+    for (const textNodesKey in textNodes) {
+      const textNode = textNodes[textNodesKey] as HTMLElement;
+      if (textNode.localName !== 'span') {
+        continue;
+      }
+      textNode.id = `${pageRenderId}-text-${textNodesKey}`
+    }
 
-
-    })
   }
 
   /**
@@ -135,9 +171,17 @@ export default function(props: any) {
     pdfViewer.currentScaleValue = scale;
   }
 
+  /**
+   * 展示缩略图
+   */
+  const showThumbnail = () => {
+
+  }
+
   return {
     renderPDF,
-    renderPDFPage,
+    changeScale,
+    showThumbnail
   }
 
 }
